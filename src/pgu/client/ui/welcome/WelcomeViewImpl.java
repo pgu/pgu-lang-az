b1c07f8a32436e224ef1d55cd80a667c6b954853
Guilcher Pascal <pgu.dev@gmail.com>
2011-11-29 22:57:15 +0100

    update..

--- src/pgu/client/ui/welcome/WelcomeViewImpl.java
+++ src/pgu/client/ui/welcome/WelcomeViewImpl.java
@@ -3,14 +3,14 @@ package pgu.client.ui.welcome;
 import pgu.client.ui.welcome.score.Score;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.logical.shared.ResizeEvent;
+import com.google.gwt.event.logical.shared.ResizeHandler;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.HTMLPanel;
-import com.google.gwt.user.client.ui.InlineLabel;
-import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.Widget;
 
 public class WelcomeViewImpl extends Composite implements WelcomeView {
@@ -21,56 +21,42 @@ public class WelcomeViewImpl extends Composite implements WelcomeView {
     private static WelcomeViewImplUiBinder uiBinder = GWT.create(WelcomeViewImplUiBinder.class);
 
     @UiField
-    HTMLPanel toolbarApp;
-    @UiField
     HTMLPanel westArea;
     @UiField
     HTMLPanel eastArea;
-    @UiField
-    HTMLPanel level;
+
     @UiField
     HTMLPanel scores;
 
     @UiField
+    HTML logo;
+    @UiField
     HTML login;
     @UiField
-    HTML google;
-    @UiField
-    HTML twitter;
-    @UiField
-    HTML facebook;
-    @UiField
-    HTML linkedin;
+    HTML level;
     @UiField
     HTML start;
 
     public WelcomeViewImpl() {
         initWidget(uiBinder.createAndBindUi(this));
 
-        login.setText("Login");
-        google.setText("google");
-        twitter.setText("twitter");
-        facebook.setText("facebook");
-        linkedin.setText("linkedin");
-        start.setText("start");
-
-        final ListBox language = new ListBox();
-        language.addItem("Japan");
-        level.add(language);
-
-        final ListBox alphabet = new ListBox();
-        alphabet.addItem("Hiragani");
-        level.add(alphabet);
-
-        final InlineLabel levelNb = new InlineLabel();
-        levelNb.setText("1");
-        level.add(levelNb);
+        logo.setText("LOGO");
+        login.setText("LOGIN");
+        level.setText("LEVEL");
+        start.setText("START");
 
         scores.add(new Score("45 sec, Jap/Hir/20, Toto"));
         scores.add(new Score("2 min 30 sec, Jap/Hir/1, Titi"));
         scores.add(new Score("10 min, Jap/Hir/30, Toto"));
         scores.add(new Score("5 min 22 sec, Jap/Hir/11, Tata"));
 
+        Window.addResizeHandler(new ResizeHandler() {
+
+            @Override
+            public void onResize(final ResizeEvent event) {
+                resize();
+            }
+        });
     }
 
     @Override
@@ -79,17 +65,34 @@ public class WelcomeViewImpl extends Composite implements WelcomeView {
 
     @Override
     public Widget asWidget() {
-
-        final int w = Window.getClientWidth();
-        final int h = Window.getClientHeight() - toolbarApp.getOffsetHeight();
-
-        final int westAreaWidth = w * 90 / 100 - 10;
-        westArea.setPixelSize(westAreaWidth, h);
-
-        final int eastAreaWidth = w * 10 / 100 - 10;
-        eastArea.setPixelSize(eastAreaWidth, h);
-
+        resize();
         return super.asWidget();
+    }
+
+    private void resize() {
+        final int w = Window.getClientWidth();
+        final int h = Window.getClientHeight();
+
+        final boolean isPortrait = w < h;
+
+        int eastAreaWidth = 0;
+        if (isPortrait) {
+            eastAreaWidth = w / 3;
+        } else {
+            eastAreaWidth = w / 4;
+        }
+
+        eastArea.setPixelSize(eastAreaWidth, h);
+        westArea.setPixelSize(w - eastAreaWidth, h);
+
+        final int hBtn = h * 30 / 100 + 4;
+        logo.setPixelSize(eastAreaWidth, h - 3 * hBtn - 13);
+
+        final int wBtn = eastAreaWidth - 4;
+        login.setPixelSize(wBtn, hBtn);
+        level.setPixelSize(wBtn, hBtn);
+        start.setPixelSize(wBtn, hBtn);
+
     }
 
 }
