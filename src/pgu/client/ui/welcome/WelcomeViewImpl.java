96166020db7cff2ba9d45a74e9a0c51961e826f9
Guilcher Pascal <pgu.dev@gmail.com>
2011-12-26 19:50:28 +0100

    adjust welcome page

--- src/pgu/client/ui/welcome/WelcomeViewImpl.java
+++ src/pgu/client/ui/welcome/WelcomeViewImpl.java
@@ -3,11 +3,14 @@ package pgu.client.ui.welcome;
 import pgu.client.ui.welcome.score.Score;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.event.logical.shared.ResizeEvent;
 import com.google.gwt.event.logical.shared.ResizeHandler;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.Window.ScrollEvent;
+import com.google.gwt.user.client.Window.ScrollHandler;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.HTMLPanel;
@@ -38,16 +41,13 @@ public class WelcomeViewImpl extends Composite implements WelcomeView {
         initWidget(uiBinder.createAndBindUi(this));
 
         // logo.setText("LOGO");
-        login.setText("LOGIN");
-        level.setText("LEVEL");
-        start.setText("START");
+        // login.setText("LOGIN");
+        // level.setText("LEVEL");
+        // start.setText("START");
 
-        for (int i = 0; i < 10; i++) {
-            listArea.add(new Score("45 sec, Jap/Hir/20, Toto"));
-            listArea.add(new Score("2 min 30 sec, Jap/Hir/1, Titi"));
-            listArea.add(new Score("10 min, Jap/Hir/30, Toto"));
-            listArea.add(new Score("5 min 22 sec, Jap/Hir/11, Tata"));
-
+        for (int i = 0; i < 100; i++) {
+            listArea.add(new Score(i + " min 30 sec, Jap/Hir/1, Toto"));
+            listArea.add(new Score(i + "0 min 30 sec, Jap/Hir/10, Titi"));
         }
 
         Window.addResizeHandler(new ResizeHandler() {
@@ -56,6 +56,16 @@ public class WelcomeViewImpl extends Composite implements WelcomeView {
             public void onResize(final ResizeEvent event) {
                 resize();
             }
+        });
+
+        Window.addWindowScrollHandler(new ScrollHandler() {
+
+            @Override
+            public void onWindowScroll(final ScrollEvent event) {
+                GWT.log(" event.getScrollTop() " + event.getScrollTop());
+                menuArea.getElement().getStyle().setTop(event.getScrollTop(), Unit.PX);
+            }
+
         });
     }
 
@@ -73,17 +83,18 @@ public class WelcomeViewImpl extends Composite implements WelcomeView {
         final int w = Window.getClientWidth();
         final int h = Window.getClientHeight();
 
-        // final boolean isPortrait = w < h;
+        final boolean isPortrait = w < h;
+        final int hMenu = isPortrait ? 200 : 100;
+        menuArea.setPixelSize(w, hMenu);
 
-        // int menuAreaWidth = 0;
-        // if (isPortrait) {
-        // menuAreaWidth = w / 3;
-        // } else {
-        // menuAreaWidth = w / 4;
-        // }
+        if (isPortrait) {
+            // TODO PGU appliquer le css de portrait pour la menu bar
+        } else {
+            // TODO PGU appliquer le css de paysage pour la menu bar
+        }
 
-        menuArea.setPixelSize(w, 50);
-        listArea.setPixelSize(w, h - 50);
+        // listArea.setPixelSize(w, h - 50);
+        listArea.setWidth(w + "px");
 
         // final int hBtn = h * 30 / 100 + 4;
         // logo.setPixelSize(menuAreaWidth, h - 3 * hBtn - 13);
@@ -91,10 +102,15 @@ public class WelcomeViewImpl extends Composite implements WelcomeView {
         // final int wBtn = menuAreaWidth - 4;
 
         final int wBtn = w / 3 - 20;
-        logo.setPixelSize(w - 3 * wBtn - 18, 40);
-        login.setPixelSize(wBtn, 40);
-        level.setPixelSize(wBtn, 40);
-        start.setPixelSize(wBtn, 40);
+        final int hBtn = hMenu - 7;
+        logo.setPixelSize(w - 3 * wBtn - 18, hBtn);
+        login.setPixelSize(wBtn, hBtn);
+        level.setPixelSize(wBtn, hBtn);
+        start.setPixelSize(wBtn, hBtn);
+
+        login.getElement().getStyle().setFontSize(6.5, Unit.EM);
+        level.getElement().getStyle().setFontSize(6.5, Unit.EM);
+        start.getElement().getStyle().setFontSize(6.5, Unit.EM);
 
         for (int i = 0; i < listArea.getWidgetCount(); i++) {
             final Score score = (Score) listArea.getWidget(i);
