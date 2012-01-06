2b4c25d53e77a210592171924aba366f30a0d9c8
Pascal Guilcher <guilcher.p@sfeir.com>
2012-01-06 10:34:46 +0100

    update...

--- src/pgu/client/language/RussianAlphabet.java
+++ src/pgu/client/language/RussianAlphabet.java
@@ -8,7 +8,7 @@ import pgu.client.myguava.HashBiMap;
 public class RussianAlphabet {
 
     private static final HashBiMap<String, String> latin2russian = HashBiMap.create();
-    private static final List<String> availableLevels;
+    private static final List<String>              availableLevels;
 
     public static List<String> availableLevels() {
         return availableLevels;
@@ -19,23 +19,61 @@ public class RussianAlphabet {
 
         for (final String selectedLevel : selectedLevels) {
 
-            if (A.equals(selectedLevel)) {
+            if (G_1.equals(selectedLevel)) {
                 availableSymbols.put("A", latin2russian.get("A"));
                 availableSymbols.put("B", latin2russian.get("B"));
                 availableSymbols.put("V", latin2russian.get("V"));
                 availableSymbols.put("G", latin2russian.get("G"));
                 availableSymbols.put("D", latin2russian.get("D"));
             }
+            if (G_2.equals(selectedLevel)) {
+                availableSymbols.put("YE", latin2russian.get("YE"));
+                availableSymbols.put("YO", latin2russian.get("YO"));
+                availableSymbols.put("&#x0290;", latin2russian.get("&#x0290;"));
+                availableSymbols.put("Z", latin2russian.get("Z"));
+                availableSymbols.put("I", latin2russian.get("I"));
+                availableSymbols.put("Y", latin2russian.get("Y"));
+            }
+            if (G_3.equals(selectedLevel)) {
+                availableSymbols.put("K", latin2russian.get("K"));
+                availableSymbols.put("L", latin2russian.get("L"));
+                availableSymbols.put("M", latin2russian.get("M"));
+                availableSymbols.put("N", latin2russian.get("N"));
+                availableSymbols.put("O", latin2russian.get("O"));
+            }
+            if (G_4.equals(selectedLevel)) {
+                availableSymbols.put("P", latin2russian.get("P"));
+                availableSymbols.put("R", latin2russian.get("R"));
+                availableSymbols.put("S", latin2russian.get("S"));
+                availableSymbols.put("T", latin2russian.get("T"));
+                availableSymbols.put("U", latin2russian.get("U"));
+                availableSymbols.put("F", latin2russian.get("F"));
+            }
+            if (G_5.equals(selectedLevel)) {
+                availableSymbols.put("X", latin2russian.get("X"));
+                availableSymbols.put("TS", latin2russian.get("TS"));
+                availableSymbols.put("CH", latin2russian.get("CH"));
+                availableSymbols.put("&#x0282;", latin2russian.get("&#x0282;"));
+                availableSymbols.put("&#x0285;", latin2russian.get("&#x0285;"));
+                availableSymbols.put("&#x0268;", latin2russian.get("&#x0268;"));
+                availableSymbols.put("E", latin2russian.get("E"));
+                availableSymbols.put("YU", latin2russian.get("YU"));
+                availableSymbols.put("YA", latin2russian.get("YA"));
+            }
         }
 
         return availableSymbols;
     }
 
-    private static final String A = "0 -";
+    private static final String G_1 = "A - D";
+    private static final String G_2 = "YE - Y";
+    private static final String G_3 = "K - O";
+    private static final String G_4 = "P - F";
+    private static final String G_5 = "X - YA";
 
     static {
-        availableLevels = Arrays.asList( //
-                A);
+        availableLevels = Arrays.asList(G_1, G_2, G_3, G_4, G_5);
+
         latin2russian.put("A", "&#x0410;");
         latin2russian.put("B", "&#x0411;");
         latin2russian.put("V", "&#x0412;");
@@ -62,7 +100,7 @@ public class RussianAlphabet {
         latin2russian.put("TS", "&#x0426;");
         latin2russian.put("CH", "&#x0427;");
         latin2russian.put("&#x0282;", "&#x0428;");
-        latin2russian.put("&#x0285;&#x0285;", "&#x0429;");
+        latin2russian.put("&#x0285;", "&#x0429;");
         latin2russian.put("&#x0268;", "&#x042B;");
         latin2russian.put("E", "&#x042D;");
         latin2russian.put("YU", "&#x042E;");
