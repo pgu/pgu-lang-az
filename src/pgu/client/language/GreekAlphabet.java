0acc3d2a005c5d93a7b1add657646f8391bf69db
Guilcher Pascal <pgu.dev@gmail.com>
2012-01-15 19:05:53 +0100

    backup

--- src/pgu/client/language/GreekAlphabet.java
+++ src/pgu/client/language/GreekAlphabet.java
@@ -17,30 +17,54 @@ public enum GreekAlphabet implements HasLevels {
     private static final String T_0M = "T - OM";
 
     {
-        latin2greek.put("A", "&#x0391;");
-        latin2greek.put("B", "&#x0392;");
-        latin2greek.put("G", "&#x0393;");
-        latin2greek.put("D", "&#x0394;");
-        latin2greek.put("E", "&#x0395;");
-        latin2greek.put("Z", "&#x0396;");
-        latin2greek.put("ET", "&#x0397;");
-        latin2greek.put("TH", "&#x0398;");
-        latin2greek.put("I", "&#x0399;");
-        latin2greek.put("K", "&#x039a;");
-        latin2greek.put("L", "&#x039b;");
-        latin2greek.put("M", "&#x039c;");
-        latin2greek.put("N", "&#x039d;");
-        latin2greek.put("X", "&#x039e;");
-        latin2greek.put("O", "&#x039f;");
-        latin2greek.put("P", "&#x03a0;");
-        latin2greek.put("R", "&#x03a1;");
-        latin2greek.put("S", "&#x03a3;");
-        latin2greek.put("T", "&#x03a4;");
-        latin2greek.put("U", "&#x03a5;");
-        latin2greek.put("PH", "&#x03a6;");
-        latin2greek.put("CH", "&#x03a7;");
-        latin2greek.put("PS", "&#x03a8;");
-        latin2greek.put("OM", "&#x03a9;");
+        latin2greek.put("ALPHA", "&#x0391;");
+        latin2greek.put("BETA", "&#x0392;");
+        latin2greek.put("GAMMA", "&#x0393;");
+        latin2greek.put("DELTA", "&#x0394;");
+        latin2greek.put("EPSILON", "&#x0395;");
+        latin2greek.put("ZETA", "&#x0396;");
+        latin2greek.put("ETA", "&#x0397;");
+        latin2greek.put("THETA", "&#x0398;");
+        latin2greek.put("IOTA", "&#x0399;");
+        latin2greek.put("KAPPA", "&#x039a;");
+        latin2greek.put("LAMBDA", "&#x039b;");
+        latin2greek.put("MU", "&#x039c;");
+        latin2greek.put("NU", "&#x039d;");
+        latin2greek.put("XI", "&#x039e;");
+        latin2greek.put("OMICRON", "&#x039f;");
+        latin2greek.put("PI", "&#x03a0;");
+        latin2greek.put("ROH", "&#x03a1;");
+        latin2greek.put("SIGMA", "&#x03a3;");
+        latin2greek.put("TAU", "&#x03a4;");
+        latin2greek.put("UPSILON", "&#x03a5;");
+        latin2greek.put("PHI", "&#x03a6;");
+        latin2greek.put("CHI", "&#x03a7;");
+        latin2greek.put("PSI", "&#x03a8;");
+        latin2greek.put("OMEGA", "&#x03a9;");
+        latin2greek.put("alpha", "&#x03B1;");
+        latin2greek.put("beta", "&#x03B2;");
+        latin2greek.put("gamma", "&#x03B3;");
+        latin2greek.put("delta", "&#x03B4;");
+        latin2greek.put("epsilon", "&#x03B5;");
+        latin2greek.put("zeta", "&#x03B6;");
+        latin2greek.put("eta", "&#x03B7;");
+        latin2greek.put("theta", "&#x03B8;");
+        latin2greek.put("iota", "&#x03B9;");
+        latin2greek.put("kappa", "&#x03BA;");
+        latin2greek.put("lambda", "&#x03BB;");
+        latin2greek.put("mu", "&#x03BC;");
+        latin2greek.put("nu", "&#x03BD;");
+        latin2greek.put("xi", "&#x03BE;");
+        latin2greek.put("omicron", "&#x03BF;");
+        latin2greek.put("pi", "&#x03C0;");
+        latin2greek.put("roh", "&#x03C1;");
+        latin2greek.put("sigma", "&#x03C3;");
+        latin2greek.put("tau", "&#x03C4;");
+        latin2greek.put("upsilon", "&#x03C5;");
+        latin2greek.put("phi", "&#x03C6;");
+        latin2greek.put("chi", "&#x03C7;");
+        latin2greek.put("psi", "&#x03C8;");
+        latin2greek.put("omega", "&#x03C9;");
     }
 
     @Override
@@ -54,36 +78,60 @@ public enum GreekAlphabet implements HasLevels {
         for (final String selectedLevel : selectedLevels) {
 
             if (A_Z.equals(selectedLevel)) {
-                availableSymbols.put("A", latin2greek.get("A"));
-                availableSymbols.put("B", latin2greek.get("B"));
-                availableSymbols.put("G", latin2greek.get("G"));
-                availableSymbols.put("D", latin2greek.get("D"));
-                availableSymbols.put("E", latin2greek.get("E"));
-                availableSymbols.put("Z", latin2greek.get("Z"));
+                availableSymbols.put("ALPHA", latin2greek.get("ALPHA"));
+                availableSymbols.put("BETA", latin2greek.get("BETA"));
+                availableSymbols.put("GAMMA", latin2greek.get("GAMMA"));
+                availableSymbols.put("DELTA", latin2greek.get("DELTA"));
+                availableSymbols.put("EPSILON", latin2greek.get("EPSILON"));
+                availableSymbols.put("ZETA", latin2greek.get("ZETA"));
+                availableSymbols.put("alpha", latin2greek.get("alpha"));
+                availableSymbols.put("beta", latin2greek.get("beta"));
+                availableSymbols.put("gamma", latin2greek.get("gamma"));
+                availableSymbols.put("delta", latin2greek.get("delta"));
+                availableSymbols.put("epsilon", latin2greek.get("epsilon"));
+                availableSymbols.put("zeta", latin2greek.get("zeta"));
 
             } else if (ET_M.equals(selectedLevel)) {
-                availableSymbols.put("ET", latin2greek.get("ET"));
-                availableSymbols.put("TH", latin2greek.get("TH"));
-                availableSymbols.put("I", latin2greek.get("I"));
-                availableSymbols.put("K", latin2greek.get("K"));
-                availableSymbols.put("L", latin2greek.get("L"));
-                availableSymbols.put("M", latin2greek.get("M"));
+                availableSymbols.put("ETA", latin2greek.get("ETA"));
+                availableSymbols.put("THETA", latin2greek.get("THETA"));
+                availableSymbols.put("IOTA", latin2greek.get("IOTA"));
+                availableSymbols.put("KAPPA", latin2greek.get("KAPPA"));
+                availableSymbols.put("LAMBDA", latin2greek.get("LAMBDA"));
+                availableSymbols.put("MU", latin2greek.get("MU"));
+                availableSymbols.put("eta", latin2greek.get("eta"));
+                availableSymbols.put("theta", latin2greek.get("theta"));
+                availableSymbols.put("iota", latin2greek.get("iota"));
+                availableSymbols.put("kappa", latin2greek.get("kappa"));
+                availableSymbols.put("lambda", latin2greek.get("lambda"));
+                availableSymbols.put("mu", latin2greek.get("mu"));
 
             } else if (N_S.equals(selectedLevel)) {
-                availableSymbols.put("N", latin2greek.get("N"));
-                availableSymbols.put("X", latin2greek.get("X"));
-                availableSymbols.put("O", latin2greek.get("O"));
-                availableSymbols.put("P", latin2greek.get("P"));
-                availableSymbols.put("R", latin2greek.get("R"));
-                availableSymbols.put("S", latin2greek.get("S"));
+                availableSymbols.put("NU", latin2greek.get("NU"));
+                availableSymbols.put("XI", latin2greek.get("XI"));
+                availableSymbols.put("OMICRON", latin2greek.get("OMICRON"));
+                availableSymbols.put("PI", latin2greek.get("PI"));
+                availableSymbols.put("ROH", latin2greek.get("ROH"));
+                availableSymbols.put("SIGMA", latin2greek.get("SIGMA"));
+                availableSymbols.put("nu", latin2greek.get("nu"));
+                availableSymbols.put("xi", latin2greek.get("xi"));
+                availableSymbols.put("omicron", latin2greek.get("omicron"));
+                availableSymbols.put("pi", latin2greek.get("pi"));
+                availableSymbols.put("roh", latin2greek.get("roh"));
+                availableSymbols.put("sigma", latin2greek.get("sigma"));
 
             } else if (T_0M.equals(selectedLevel)) {
-                availableSymbols.put("T", latin2greek.get("T"));
-                availableSymbols.put("U", latin2greek.get("U"));
-                availableSymbols.put("PH", latin2greek.get("PH"));
-                availableSymbols.put("CH", latin2greek.get("CH"));
-                availableSymbols.put("PS", latin2greek.get("PS"));
-                availableSymbols.put("OM", latin2greek.get("OM"));
+                availableSymbols.put("TAU", latin2greek.get("TAU"));
+                availableSymbols.put("UPSILON", latin2greek.get("UPSILON"));
+                availableSymbols.put("PHI", latin2greek.get("PHI"));
+                availableSymbols.put("CHI", latin2greek.get("CHI"));
+                availableSymbols.put("PSI", latin2greek.get("PSI"));
+                availableSymbols.put("OMEGA", latin2greek.get("OMEGA"));
+                availableSymbols.put("tau", latin2greek.get("tau"));
+                availableSymbols.put("upsilon", latin2greek.get("upsilon"));
+                availableSymbols.put("phi", latin2greek.get("phi"));
+                availableSymbols.put("chi", latin2greek.get("chi"));
+                availableSymbols.put("psi", latin2greek.get("psi"));
+                availableSymbols.put("omega", latin2greek.get("omega"));
 
             } else {
                 throw new IllegalArgumentException("Unknown level: " + selectedLevel);
