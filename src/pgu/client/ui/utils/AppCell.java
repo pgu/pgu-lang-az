23c71fe96e7fa1b53f514ca20e9b84e4b5d4ef45
Guilcher Pascal <pgu.dev@gmail.com>
2012-02-26 19:26:54 +0100

    add flash cell

---
+++ src/pgu/client/ui/utils/AppCell.java
@@ -0,0 +1,51 @@
+package pgu.client.ui.utils;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HTMLPanel;
+import com.google.gwt.user.client.ui.Widget;
+
+public class AppCell extends Composite {
+
+    private static AppCellUiBinder uiBinder = GWT.create(AppCellUiBinder.class);
+
+    interface AppCellUiBinder extends UiBinder<Widget, AppCell> {
+    }
+
+    @UiField
+    HTMLPanel cellExt2, cellExt1, cellExt, cellMed, cellInt, cellInt1, cellInt2;
+    @UiField
+    HTML cellText;
+
+    public AppCell() {
+        initWidget(uiBinder.createAndBindUi(this));
+    }
+
+    public void setText(final String text) {
+        cellText.setText(text);
+    }
+
+    public void setSize(final int width, final int height) {
+        int h = height;
+
+        cellExt2.setSize(width + "px", h + "px");
+        cellExt1.setHeight(h + "px");
+        h = h - 2 * 1;
+
+        cellExt.setHeight(h + "px");
+        h = h - 2 * 3;
+
+        cellMed.setHeight(h + "px");
+        h = h - 2 * 1;
+
+        cellInt.setHeight(h + "px");
+
+        cellInt1.setHeight(h + "px");
+        cellInt2.setHeight(h + "px");
+
+    }
+
+}
